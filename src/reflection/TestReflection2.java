e531e3204a7cbed04ff1df5909a000592a7595a4
Dante <Dante@DESKTOP-J7KBQ5V>
2018-08-15 22:32:54 +0800

    test

--- src/reflection/TestReflection2.java
+++ src/reflection/TestReflection2.java
@@ -10,7 +10,7 @@ public class TestReflection2 {
                 TestReflection2.method1();
             }
         };
-        t1.setName("第一个线程");
+        t1.setName("第一个线程+改一改");
         t1.start();
          
         //保证第一个线程先调用method1
